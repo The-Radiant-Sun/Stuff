73700dd23b819f510a3a47deb0b3b3fa23931439
The-Radiant-Sun <65923244+The-Radiant-Sun@users.noreply.github.com>
2021-12-06 10:53:48 +1300

    Creating main

---
+++ main.py
@@ -0,0 +1,6 @@
+"""from Farey_Algorithm import Farey_Algorithm
+
+FA = Farey_Algorithm(int(input("Input cycle limit: ")))
+values = FA.convert(input("Input value for fractional approximation: "))
+print(f"The approximated fraction is {values[0]}/{values[1]}, with a decimal conversion of {values[2]}")"""
+
