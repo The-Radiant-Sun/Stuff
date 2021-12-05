4e5c39cf67f8ec402b426a35b32646298b6c3d67
The-Radiant-Sun <65923244+The-Radiant-Sun@users.noreply.github.com>
2021-12-06 10:53:21 +1300

    Adding Farey Algorithn

---
+++ Farey_Algorithm.py
@@ -0,0 +1,26 @@
+class Farey_Algorithm:
+    def __init__(self, iterations):
+        self.iterations = iterations
+
+    def convert(self, integer):
+        sNum = integer.split('.')
+        wNum = int(sNum[0])
+        dNum = float(integer) - wNum
+
+        uNumer, uDenom = 1, 1
+        lNumer, lDenom = 0, 1
+
+        for i in range(self.iterations):
+            nNumer = uNumer + lNumer
+            nDenom = uDenom + lDenom
+            if nNumer / nDenom < dNum:
+                lNumer, lDenom = nNumer, nDenom
+            elif nNumer / nDenom > dNum:
+                uNumer, uDenom = nNumer, nDenom
+            else:
+                return [nNumer, nDenom, nNumer / nDenom]
+
+        uNumer = uNumer + uDenom * wNum
+        lNumer = lNumer + lDenom * wNum
+
+        return [uNumer, uDenom, uNumer / uDenom] if abs(uNumer / uDenom - dNum) < abs(lNumer / lDenom - dNum) else [lNumer, lDenom, lNumer / lDenom]
