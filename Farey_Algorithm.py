8373115e01b806d47e8a454c11fc1a83ca119862
The-Radiant-Sun <65923244+The-Radiant-Sun@users.noreply.github.com>
2021-12-06 16:46:33 +1300

    Commenting Farey_Algorithm

--- Farey_Algorithm.py
+++ Farey_Algorithm.py
@@ -2,25 +2,25 @@ class Farey_Algorithm:
     def __init__(self, limit):
         self.limit = limit
 
-    def convert(self, integer):
-        sNum = integer.split('.')
+    def convert(self, rational):
+        sNum = rational.split('.')
         wNum = int(sNum[0])
-        dNum = float(integer) - wNum
+        dNum = float(rational) - wNum  # Converting the inputted value into a 0 - 1 decimal
 
-        uNumer, uDenom = 1, 1
-        lNumer, lDenom = 0, 1
+        uNumer, uDenom = 1, 1  # Creating upper bound fraction
+        lNumer, lDenom = 0, 1  # Creating lower bound fraction
 
         for i in range(self.limit):
-            nNumer = uNumer + lNumer
-            nDenom = uDenom + lDenom
+            nNumer = uNumer + lNumer  # Creating median fraction numerator
+            nDenom = uDenom + lDenom  # Creating median fraction denominator
             if nNumer / nDenom < dNum:
-                lNumer, lDenom = nNumer, nDenom
+                lNumer, lDenom = nNumer, nDenom  # Replacing the lower bound if possible
             elif nNumer / nDenom > dNum:
-                uNumer, uDenom = nNumer, nDenom
+                uNumer, uDenom = nNumer, nDenom  # Replacing the upper bound if possible
             else:
                 return [nNumer, nDenom, nNumer / nDenom]
 
-        uNumer = uNumer + uDenom * wNum
+        uNumer = uNumer + uDenom * wNum  # Reformating into rational number
         lNumer = lNumer + lDenom * wNum
 
         return [uNumer, uDenom, uNumer / uDenom] if abs(uNumer / uDenom - dNum) < abs(lNumer / lDenom - dNum) else [lNumer, lDenom, lNumer / lDenom]
