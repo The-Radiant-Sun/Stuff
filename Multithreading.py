6fb4cdd0fe90139ba9c9a822f4803c3eda50c5ad
The-Radiant-Sun <65923244+The-Radiant-Sun@users.noreply.github.com>
2021-12-11 17:57:29 +1300

    Multithreading 2

--- Multithreading.py
+++ Multithreading.py
@@ -1,22 +1,54 @@
 import threading
 
-class Mulitithreading:
+class Multithreading:
     def __init__(self):
-        self.threads = {}
+        self.threads = {}  # Create dictionary of all names and threads for future calling
 
-    def openThread(self, name: str, function, arguments: tuple):
+    def openInactiveThread(self, name: str, function, arguments: tuple):
         if name in self.threads.keys():
-            print(f'Overwriting previous {name} thread')
+            print(f'Overwriting previous {name} thread')  # Notify user if overwriting thread
 
-        self.threads[name] = threading.Thread(target=function, args=arguments)
+        self.threads[name] = threading.Thread(target=function, args=arguments, name=name)  # Pass values into threaded function
 
-    def runAllThreads(self):
+    def openActiveThread(self, name: str, function, arguments: tuple):
+        self.openInactiveThread(name, function, arguments)
+        self.threads[name].start()  # Activating thread
+
+    def isThreadAlive(self, name: str):
+        return (True if self.threads[name].is_alive() else False) if name in self.threads.keys() else False
+
+    def runAllThreads(self, join: bool):
         for thread in self.threads.values():
-            thread.start()
+            if thread.is_alive():
+                thread.start()
+                if join:
+                    thread.join()  # Prevents the main thread from closing until all joined threads have closed
+            else:
+                del self.threads[thread.name]
 
-    def runThreads(self, name: str):
-        if name in self.threads.keys():
-            self.threads[name].start()
+    def runThreads(self, names: list[str], join: list[bool]):
+        if len(names) != len(join):
+            print('List length error')
         else:
-            print(f'{name} not found')
+            for i in range(len(names)):
+                self.runThread(names[i], join[i])
 
+    def runThread(self, name: str, join: bool):
+        if self.isThreadAlive(name):
+            self.threads[name].start()  # Find and start specific thread if found
+            if join:
+                self.threads[name].join()
+        else:
+            print(f'{name} not active')  # Notify user of thread absence
+
+    def convertDaemon(self, name: str, status):
+        if self.isThreadAlive(name):
+            self.threads[name].setDaemon(status)  # Set thread to also be closed by the main thread
+
+    def pruneInactiveThreads(self):
+        remove = []
+        for thread in self.threads:
+            if not self.isThreadAlive(thread):
+                remove.append(thread)  # Remove inactive threads
+        for thread in remove:
+            del self.threads[thread]
--- main.py
+++ main.py
@@ -7,7 +7,20 @@ FA = Farey_Algorithm(requestPRN("Input cycle limit: "))
 values = FA.convert(input("Input value for fractional approximation: "))
 print(f"The approximated fraction is {values[0]}/{values[1]}, with a decimal conversion of {values[2]}")"""
 
-from Hailstone_Algorithm import Hailstone_Algorithm
+"""from Hailstone_Algorithm import Hailstone_Algorithm
 
 HA = Hailstone_Algorithm(requestPRN("Input cycle limit: "))
-HA.reduce(requestPRN("Input values: "))
+HA.reduce(requestPRN("Input values: "))"""
+
+from Multithreading import Multithreading
+MT = Multithreading()
+
+
+def p(n):
+    print(f"Thread {n} active")
+
+
+for i in range(5):
+    MT.openActiveThread(str(i), p, (i,))
+
+
