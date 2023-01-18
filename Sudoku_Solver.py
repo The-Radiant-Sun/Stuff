15f5891dc6c40fffd65079d714f43fbd270a9777
The-Radiant-Sun <65923244+The-Radiant-Sun@users.noreply.github.com>
2023-01-18 14:57:31 +1300

    Optimise

--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -1,42 +1,36 @@
 class Sudoku_Solver:
-    def __init__(self):
-        self.board = [[5, 3, 0,  0, 7, 0,  0, 0, 0],
-                      [0, 0, 0,  1, 9, 5,  0, 0, 0],
-                      [0, 9, 8,  0, 0, 0,  0, 6, 0],
+    def __init__(self, board):
+        self.board = board
+        self.rows = [set(range(1, 10)) for _ in range(9)]
+        self.cols = [set(range(1, 10)) for _ in range(9)]
+        self.squares = [set(range(1, 10)) for _ in range(9)]
 
-                      [8, 0, 0,  0, 6, 0,  0, 0, 3],
-                      [4, 0, 0,  8, 0, 0,  0, 0, 1],
-                      [0, 0, 0,  0, 2, 0,  0, 0, 0],
-
-                      [0, 6, 0,  0, 0, 0,  2, 8, 0],
-                      [0, 0, 0,  4, 0, 9,  0, 0, 5],
-                      [0, 0, 0,  0, 8, 0,  0, 7, 0]]
+        for row in range(9):
+            for col in range(9):
+                if self.board[row][col] != 0:
+                    num = self.board[row][col]
+                    self.rows[row].remove(num)
+                    self.cols[col].remove(num)
+                    squareIndex = (row // 3) * 3 + (col // 3)
+                    self.squares[squareIndex].remove(num)
 
     def solve(self):
         # Find the next empty cell
         for row in range(9):
             for col in range(9):
                 if self.board[row][col] == 0:
-                    for num in range(1, 10):
-                        if self.is_valid(row, col, num):
-                            self.board[row][col] = num
-                            self.display()
-                            print("")
-                            if self.solve():
-                                return True
-                            self.board[row][col] = 0
-                    return False
-        return True
-
-    def is_valid(self, row, col, num):
-        # Check if the number is valid in the current cell
-        for x in range(9):
-            if self.board[row][x] == num or self.board[x][col] == num:
-                return False
-        start_row, start_col = row - row % 3, col - col % 3
-        for i in range(3):
-            for j in range(3):
-                if self.board[i + start_row][j + start_col] == num:
+                    squareIndex = (row // 3) * 3 + (col // 3)
+                    for num in self.rows[row].intersection(self.cols[col], self.squares[squareIndex]):
+                        self.board[row][col] = num
+                        self.rows[row].remove(num)
+                        self.cols[col].remove(num)
+                        self.squares[squareIndex].remove(num)
+                        if self.solve():
+                            return True
+                        self.board[row][col] = 0
+                        self.rows[row].add(num)
+                        self.cols[col].add(num)
+                        self.squares[squareIndex].add(num)
                     return False
         return True
 
--- main.py
+++ main.py
@@ -25,7 +25,20 @@ for i in range(5):
 File_Type_Converter.convertFileTypesInFolders('/Users/finn/Documents/PycharmProjects/Jump-Recorder/Info', 'csv', 'txt')"""
 
 from Sudoku_Solver import Sudoku_Solver
-sudoku = Sudoku_Solver()
+
+board = [[5, 3, 0,  0, 7, 0,  0, 0, 0],
+         [0, 0, 0,  1, 9, 5,  0, 0, 0],
+         [0, 9, 8,  0, 0, 0,  0, 6, 0],
+
+         [8, 0, 0,  0, 6, 0,  0, 0, 3],
+         [4, 0, 0,  8, 0, 0,  0, 0, 1],
+         [0, 0, 0,  0, 2, 0,  0, 0, 0],
+
+         [0, 6, 0,  0, 0, 0,  2, 8, 0],
+         [0, 0, 0,  4, 0, 9,  0, 0, 5],
+         [0, 0, 0,  0, 8, 0,  0, 7, 0]]
+
+sudoku = Sudoku_Solver(board)
 sudoku.solve()
 sudoku.display()
 
